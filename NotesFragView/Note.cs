bb0a0d95b19137e5b95d0fe490b7c14ff74958cf
Virko <virko.veelmaa@tptlive.ee>
2019-01-24 09:47:09 +0200

    Commit
    things are more broken now :smile:

--- NotesFragView/Note.cs
+++ NotesFragView/Note.cs
@@ -9,11 +9,13 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using SQLite;
 
 namespace NotesFragView
 {
     public class Note
     {
+        [PrimaryKey, AutoIncrement, Column("_id")]
         public int Id { get; set; }
         public string NoteTitle { get; set; }
         public string NoteContent { get; set; }
